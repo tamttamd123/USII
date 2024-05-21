d946415925b06ecd8e57b88ffadbb3dae49ced66
tamttamd123 <tamkhungbk1@gmail.com>
2024-05-21 11:35:42 +0700

    project usi

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// owl carousel 
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+function toggleSearchBar() {
+    var searchBar = document.querySelector('.search-bar');
+    searchBar.style.display = (searchBar.style.display === 'none' || searchBar.style.display === '') ? 'block' : 'none';
+}
